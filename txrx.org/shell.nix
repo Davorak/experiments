a7a99e92d19aff36bffbb5c514df3ae474881621
Patrick John Wheeler <Patrick.John.Wheeler@gmail.com>
2015-05-18 00:37:29 +0000

    Add start of nix-shell script for txrx.org site.

---
+++ txrx.org/shell.nix
@@ -0,0 +1,44 @@
+# This uses nix-shell to install the major pacakges but lets virtualenv and pip
+# take care of everything in the config/requirments.txt file.
+{ pkgs ? (import <nixpkgs> {}).pkgs }:
+
+let
+
+ pythonPackages = pkgs.pythonPackages;
+
+in
+
+pkgs.stdenv.mkDerivation {
+  name = "hello-world-wide-web";
+  buildInputs = [
+
+    pythonPackages.python
+    pythonPackages.pip
+    pythonPackages.virtualenv
+    pythonPackages.psycopg2
+    pythonPackages.pil
+#    pythonPackages.pythonPackages.django_1_5
+    pythonPackages.django_1_4
+#    pythonPackages.django
+
+    pkgs.libpng
+    pkgs.libjpeg
+    pkgs.gitAndTools.gitFull
+  ];
+#    echo "Remove virtualenv if it exists"
+#    rm -r nixShellVirtualEnv
+  shellHook = ''
+    echo "Creat new python virtualenv"
+    virtualenv nixShellVirtualEnv14
+    source nixShellVirtualEnv14/bin/activate
+
+    echo "pip install depends"
+    pip install -r config/requirements.txt
+
+    pip install django-contrib-comments
+    pip install django-markdown-deux
+    pip install django-bootstrap-form
+    pip install django-suit
+    pip install south
+  '';
+}
