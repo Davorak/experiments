e72cc3b9afeeba633cd026f5c83df645806575a2
Patrick John Wheeler <Patrick.John.Wheeler@gmail.com>
2015-07-16 20:53:45 -0500

    Init commit for DefaultRoute bug

--- examples/defaultRouteBug/src/DefaultApp.js
+++ examples/defaultRouteBug/src/DefaultApp.js
@@ -3,10 +3,7 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello, world.</h1>
-        <RouteHandler />
-      </div>
+      <h1>Default App</h1>
     );
   }
 }
--- examples/defaultRouteBug/src/index.js
+++ examples/defaultRouteBug/src/index.js
@@ -1,4 +1,17 @@
 import React from 'react';
 import App from './App';
 
-React.render(<App />, document.getElementById('root'));
+import DefaultApp from './DefaultApp';
+
+import Router, { Route, DefaultRoute } from 'react-router';
+
+var routes = (
+    <Route path="/" handler={App}>
+        <DefaultRoute handler={DefaultApp} />
+    </Route>
+);
+
+Router.run(routes, function(Handler) {
+    React.render(<Handler />, document.body);
+})
+
