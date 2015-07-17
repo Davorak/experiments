34881acc645e34d472913c4418b5421f6a74f761
Patrick John Wheeler <Patrick.John.Wheeler@gmail.com>
2015-07-16 23:26:54 -0500

    This seems show nonintuitive DefaultRouting behavior

--- examples/defaultRouteBug/src/App.js
+++ examples/defaultRouteBug/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 
 import { RouteHandler } from 'react-router';
 
-
 export default class App extends Component {
   render() {
     return (
--- examples/defaultRouteBug/src/index.js
+++ examples/defaultRouteBug/src/index.js
@@ -3,15 +3,21 @@ import App from './App';
 
 import DefaultApp from './DefaultApp';
 
+import LoggedIn from './LoggedIn';
+
 import Router, { Route, DefaultRoute } from 'react-router';
 
 var routes = (
-    <Route path="/" handler={App}>
-        <DefaultRoute handler={DefaultApp} />
+  <Route path="/" handler={App}>
+    <DefaultRoute handler={DefaultApp} />
+    <Route handler={DefaultApp} path="hi" />
+    <Route handler={LoggedIn}>
+      <Route name="nestedTesting" handler={DefaultApp} path="hi" />
     </Route>
+  </Route>
 );
 
-Router.run(routes, function(Handler) {
-    React.render(<Handler />, document.body);
+Router.run(routes, Router.HistoryLocation, function(Handler, state) {
+  React.render(<Handler a="a" />, document.getElementById('root'));
 })
 
