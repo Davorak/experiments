9fc8331d612f7dfeef803a49754b3b82c7f4208e
Patrick John Wheeler <Patrick.John.Wheeler@gmail.com>
2015-10-28 12:15:01 -0500

    Use extract text plugin to make a styles.css file and source map for production, though I might not always want a source map in production depending on its size.

--- examples/sassLoader/webpack.config.prod.js
+++ examples/sassLoader/webpack.config.prod.js
@@ -1,6 +1,8 @@
 var path = require('path');
 var webpack = require('webpack');
 
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 module.exports = {
   devtool: 'source-map',
   entry: [
@@ -22,13 +24,20 @@ module.exports = {
       compressor: {
         warnings: false
       }
-    })
+    }),
+    new ExtractTextPlugin("styles.css")
   ],
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'src')
-    }]
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('css?sourceMap!sass?sourceMap')
+      }
+    ]
   }
 };
